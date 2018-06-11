164fd1b0f47fc70e6eb46aa82c25abae8f486e94
DESKTOP-GVSQ393\jrroy <jake.resplendent@gmail.com>
2018-06-11 14:12:08 -0600

    addition 1

---
+++ ReTank/Source/ReTank/ReTankGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "ReTankGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RETANK_API AReTankGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
